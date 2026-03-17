e8770e16462662a7f4d271a707c83d88ec299c87
jacobmoneda <104470514+jacobmoneda@users.noreply.github.com>
2026-03-18 10:48:42 +1300

    next.js install, folder set up
    added map, user folders

---
+++ foodloop/app/map/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function MapPage() {
+    return (
+        <div>
+            <h1>hello world</h1>
+        </div>
+    );
+}
---
+++ foodloop/app/user/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function UserPage() {
+    return (
+        <div>
+            <h1>hello world</h1>
+        </div>
+    );
+}
